5857c7892f7bf375dc832b4847a3534665c578b0
silviomeireles <professorsilviomeireles@gmail.com>
2025-01-25 11:46:33 -0300

    Corrige dados semente e Docker.

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -1,12 +1,30 @@
 # db/seeds/users.rb
-if User.count.zero?
-  admin_profile = Profile.find_by(name: 'Administrador')
+puts "Seeding users..."
 
-  admin_user = User.create!(
+admin_profile = Profile.find_by(name: 'Administrador')
+
+users_data = [
+  {
     name: 'Administrador',
     email: 'admin@silvia.com',
-    password_digest: BCrypt::Password.create('senha123'),
+    password: 'senha123',
+    password_confirmation: 'senha123',
     profile: admin_profile,
     active: true
-  )
+  }
+  # outros usuários...
+]
+
+users_data.each do |user_data|
+  begin
+    user = User.find_by(email: user_data[:email])
+    if user
+      puts "Usuário #{user_data[:email]} já existe."
+    else
+      User.create!(user_data)
+      puts "Usuário #{user_data[:email]} criado com sucesso."
+    end
+  rescue => e
+    puts "Erro ao criar usuário #{user_data[:email]}: #{e.message}"
+  end
 end
